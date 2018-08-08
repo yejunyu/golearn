12657580b6aa3e988b97a47ec29aa0323c69a165
yejunyu <liuxing_yue@126.com>
2018-08-09 00:12:47 +0800

    函数式编程

--- lesson8/tree/tree.go
+++ lesson8/tree/tree.go
@@ -7,10 +7,6 @@ type Node struct {
 	Left, Right *Node
 }
 
-func (node Node) Print() {
-	fmt.Println(node.Value)
-}
-
 func CreateNode(value int) *Node {
 	// go 里不需要关心变量是在堆上创建还是栈上创建的
 	return &Node{Value: value}
@@ -20,12 +16,31 @@ func (node *Node) SetValue(value int) {
 	node.Value = value
 }
 
+func (node Node) Print() {
+	fmt.Println(node.Value)
+}
+
 func (node *Node) Traverse() {
+	//if node == nil {
+	//	return
+	//}
+	//// 其他语言需要判断 null,go 不用
+	//node.Left.Traverse()
+	//node.Print()
+	//node.Right.Traverse()
+
+	// 函数式编程
+	node.TraverseFunc(func(node *Node) {
+		node.Print()
+	})
+	fmt.Println()
+}
+
+func (node *Node) TraverseFunc(f func(*Node)) {
 	if node == nil {
 		return
 	}
-	// 其他语言需要判断 null,go 不用
-	node.Left.Traverse()
-	node.Print()
-	node.Right.Traverse()
+	node.Left.TraverseFunc(f)
+	f(node)
+	node.Right.TraverseFunc(f)
 }
